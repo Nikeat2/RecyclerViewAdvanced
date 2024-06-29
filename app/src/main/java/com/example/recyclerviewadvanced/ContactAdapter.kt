6435d4f5f1435b3d196ec1a0ff16ec782cdffdaf
Lobzev Nikita <lobzev.n@yandex.ru>
2024-06-29 23:12:36 +0300

    Убрал лишние строки

--- app/src/main/java/com/example/recyclerviewadvanced/ContactAdapter.kt
+++ app/src/main/java/com/example/recyclerviewadvanced/ContactAdapter.kt
@@ -1,33 +1,25 @@
 package com.example.recyclerviewadvanced
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import kotlin.coroutines.coroutineContext
 
 
 class ContactAdapter(private val contactList: MutableList<Contacts>) :
     RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
+    private lateinit var mListener: onItemClickListener
 
-
-        private lateinit var mListener: onItemClickListener
-
-        interface onItemClickListener{
-            fun onClick(position: Int)
-        }
-
-    fun setOnItemClickListener (listener: onItemClickListener) {
-        mListener = listener
+    interface onItemClickListener {
+        fun onClick(position: Int)
     }
 
-
+    fun setOnItemClickListener(listener: onItemClickListener) {
+        mListener = listener
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val context = parent.context
@@ -39,19 +31,16 @@ class ContactAdapter(private val contactList: MutableList<Contacts>) :
         return contactList.size
     }
 
-
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val currentContact = contactList.get(holder.adapterPosition)
         holder.tvName.text = currentContact.name
         holder.tvSurname.text = currentContact.surname
         holder.tvNumber.text = currentContact.phoneNumber
         Glide.with(holder.itemView.context).load(currentContact.photo).into(holder.contactImage)
-
-
     }
 
-    inner class ViewHolder(view: View, listener: onItemClickListener) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(view: View, listener: onItemClickListener) :
+        RecyclerView.ViewHolder(view) {
         val contactImage: ImageView = view.findViewById(R.id.ivPhoto)
         val tvSurname: TextView = view.findViewById(R.id.tvSurname)
         val tvName: TextView = view.findViewById(R.id.tvName)
@@ -61,8 +50,6 @@ class ContactAdapter(private val contactList: MutableList<Contacts>) :
             itemView.setOnClickListener {
                 listener.onClick(adapterPosition)
             }
-
-        }
-
         }
     }
+}
--- app/src/main/java/com/example/recyclerviewadvanced/FragmentList.kt
+++ app/src/main/java/com/example/recyclerviewadvanced/FragmentList.kt
@@ -1,39 +1,19 @@
 package com.example.recyclerviewadvanced
 
-import android.content.Context
-import android.icu.text.Transliterator.Position
-import android.media.RouteListingPreference.Item
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 
 
 class FragmentList : Fragment() {
 
-    private lateinit var contactImage: Array<String>
-    private lateinit var contactName: Array<String>
-    private lateinit var contactSurname: Array<String>
-    private lateinit var contactNumber: Array<String>
     private lateinit var newContactList: MutableList<Contacts>
     private lateinit var adapter: ContactAdapter
-
     private lateinit var fragmentDetails: ContactDetailsFragment
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-
-
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,24 +21,20 @@ class FragmentList : Fragment() {
     ): View? {
         dataInitialize()
 
-        val view =  inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
 
         return view
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         adapter = ContactAdapter(newContactList)
-
-
-
         val layoutManager = LinearLayoutManager(context)
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = adapter
 
 
-        adapter.setOnItemClickListener(object : ContactAdapter.onItemClickListener{
+        adapter.setOnItemClickListener(object : ContactAdapter.onItemClickListener {
             override fun onClick(position: Int) {
 
                 val contactImage = newContactList[position].photo
@@ -69,45 +45,41 @@ class FragmentList : Fragment() {
                 goToDetailsFragment(
                     contactImage = contactImage,
                     contactName = contactName,
-                    contactNumber = contactNumber ,
+                    contactNumber = contactNumber,
                     contactSurname = contactSurname
                 )
             }
-            })
-
-        }
-
-
-
+        })
+    }
 
     private fun dataInitialize() {
         newContactList = mutableListOf<Contacts>()
 
+        val contact1 = Contacts(
+            "Petya", "Ivanov", "89191689202",
+            "https://content.api.news/v3/images/bin/9ad43a263d501ec015ac6ab4868edf25"
+        )
 
-        val contact1 = Contacts("Petya", "Ivanov", "89191689202",
-            "https://content.api.news/v3/images/bin/9ad43a263d501ec015ac6ab4868edf25")
-
-        val contact2 = Contacts( "Vasya", "Ivanov", "89192223331",
-            "https://www.befunky.com/images/prismic/5ddfea42-7377-4bef-9ac4-f3bd407d52ab_landing-photo-to-cartoon-img5.jpeg?auto=avif,webp&format=jpg&width=863" )
-        val contact3 = Contacts("Anton", "Petrov", "89103245982", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShBJIEXRlPyClttjiGNnTAQeFnKwl2Yv0oig&s")
-
+        val contact2 = Contacts(
+            "Vasya", "Ivanov", "89192223331",
+            "https://www.befunky.com/images/prismic/5ddfea42-7377-4bef-9ac4-f3bd407d52ab_landing-photo-to-cartoon-img5.jpeg?auto=avif,webp&format=jpg&width=863"
+        )
+        val contact3 = Contacts(
+            "Anton",
+            "Petrov",
+            "89103245982",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShBJIEXRlPyClttjiGNnTAQeFnKwl2Yv0oig&s"
+        )
 
         newContactList.add(0, contact1)
         newContactList.add(1, contact2)
         newContactList.add(contact3)
-
-
-
-
     }
-
-
 
     private fun goToDetailsFragment(
         contactImage: String, contactSurname: String,
         contactName: String, contactNumber: String
     ) {
-
         fragmentDetails = ContactDetailsFragment.newInstance(
             contactImage = contactImage,
             contactName = contactName,
@@ -119,7 +91,6 @@ class FragmentList : Fragment() {
             .addToBackStack("ContactDetailsFragment.TAG")
             .commit()
     }
-
 }
 
 
